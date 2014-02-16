a84979975779d89e9419204637e220963ca90d36
zzhan133 <tmzhangzhe@gmail.com>
2014-02-16 13:53:40 -0700

    Example2-3

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -2,33 +2,44 @@
 //
 
 #include "stdafx.h"
+#include "cv.h"
 #include "highgui.h"
+
+
+int g_slider_position = 0;
+CvCapture* g_capture = NULL;
+
+void onTrackbarSlide(int pos){
+	cvSetCaptureProperty(g_capture, CV_CAP_PROP_POS_FRAMES,pos);
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-/*	const char* imagename = "baboon.jpg"; //the input image location 
-	cv::Mat img = cv::imread(imagename); 
-	if(img.empty()) 
-		return -1; 
-	if(!img.data) 
-		return -1; 
-	cv::namedWindow("image", CV_WINDOW_AUTOSIZE); 
-	cv::imshow("image", img); 
-	cv::waitKey();
-*/
-//	IplImage* img = cvLoadImage("baboon.jpg");
-	cvNamedWindow("Example2", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateFileCapture("test.avi");
+	cvNamedWindow("Example3", CV_WINDOW_AUTOSIZE);
+	g_capture = cvCreateFileCapture("test.avi");
+	int frames = (int) cvGetCaptureProperty(
+		g_capture,
+		CV_CAP_PROP_FRAME_COUNT
+	);
+	if(frames != 0) {
+		cvCreateTrackbar(
+			"position",
+			"Example3",
+			&g_slider_position,
+			frames,
+			onTrackbarSlide
+			);
+	}
 	IplImage* frame;
 	while(1){
-		frame = cvQueryFrame(capture);
+		frame = cvQueryFrame(g_capture);
 		if(!frame) break;
-		cvShowImage("Example2", frame);
+		cvShowImage("Example3", frame);
 		char c = cvWaitKey(33);
 		if(c==27) break;
 	}
-	cvReleaseCapture(&capture);
-	cvDestroyWindow("Example2");
+	cvReleaseCapture(&g_capture);
+	cvDestroyWindow("Example3");
 	return 0;
 }
 
